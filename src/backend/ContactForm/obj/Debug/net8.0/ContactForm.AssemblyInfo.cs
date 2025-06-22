229cb848932559c4eebf00d8f8cbda3f1032fba0
Jorge Sandoval <sanndovalpe.jorgeluis735@outlook.com>
2025-06-22 11:11:51 -0600

    actualización del dockerfile

--- src/backend/ContactForm/obj/Debug/net8.0/ContactForm.AssemblyInfo.cs
+++ src/backend/ContactForm/obj/Debug/net8.0/ContactForm.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContactForm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b21d73d22636ddfdffa4927bc998130d3974461")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d9dfa62bc27b6bf6025f7bdf63a97b0f503e3c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContactForm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContactForm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
